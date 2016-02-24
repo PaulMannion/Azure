043726a48e6579f880644717ee12e162dfd16055
PaulMannion <pauld.mannion@gmail.com>
2016-02-24 15:02:37 +0000

    blah

--- week4/index.php
+++ week4/index.php
@@ -11,7 +11,20 @@
         echo "<br>";
         echo 5 * 7;
         echo "<br>";
-
+        $myname = "Paul";
+        $myage = 111;
+        if ($myage >=21){
+            print "Hello " . $myname . " you are " . $myage . " so you can buy specs, mugs and sausage rolls";
+        }
+        elseif ($myage >=18) {
+            print "Hello " . $myname . " you are " . $myage . " so you can buy specs and mugs.";
+        }
+        elseif ($myage >=16) {
+            print "Hello " . $myname . " you are " . $myage . " so you can buy specs";
+        }
+        else {
+            print "Hello " . $myname . " you are " . $myage . " too young to buy anything!";
+        }
         ?>
     </p>
     </body>
