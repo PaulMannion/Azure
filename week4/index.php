4d4de720d08e82a4f1ae7fde505d197fde2dce5c
PaulMannion <pauld.mannion@gmail.com>
2016-02-24 14:38:43 +0000

    blah

--- week4/index.php
+++ week4/index.php
@@ -6,7 +6,10 @@
     <p>
         <?php
         echo "Hello World";
+        echo "<br>";
         echo "Hello,"." "."world"."!";
+        echo "<br>";
+        echo 5 * 7;
         ?>
     </p>
     </body>
