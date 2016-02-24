a4dca3176d3a918d50cf948b1a999567dda9ba75
PaulMannion <pauld.mannion@gmail.com>
2016-02-24 15:09:38 +0000

    blah

--- week4/index.php
+++ week4/index.php
@@ -11,6 +11,7 @@
         echo "<br>";
         echo 5 * 7;
         echo "<br>";
+        // Test age
         $myname = "Paul";
         $myage = 19;
         if ($myage >=21){
@@ -25,6 +26,23 @@
         else {
             print "Hello " . $myname . " you are only" . $myage . ". Too young to buy anything!";
         }
+        // Switch display wanted goods
+        $wantedgood = "mugs";
+
+        switch ($wantedgood) {
+            case "specs":
+                echo "You have to be 16 or over to buy specs";
+                break;
+            case "mugs":
+                echo "You have to be 18 or over to buy mugs";
+                break;
+            case "sausage rolls":
+                echo "You have to be 21 or over to buy sausage rolls";
+                break;
+            default:
+                echo "You don't want anything!?";
+        }
+
         ?>
     </p>
     </body>
