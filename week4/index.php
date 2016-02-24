8a4a4e61c2476ebe4d831e597fef021ac8288708
PaulMannion <pauld.mannion@gmail.com>
2016-02-24 14:28:44 +0000

    blah

---
+++ week4/index.php
@@ -0,0 +1,20 @@
+/**
+* Created by PhpStorm.
+* User: 1506100
+* Date: 24/02/2016
+* Time: 14:24
+*/
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <p>
+        <?php
+        echo "Hello World";
+        ?>
+    </p>
+    </body>
+</html>
+
+
