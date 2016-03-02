51705ba27413b20aca39bbba7b1cff2a44500459
PaulMannion <pauld.mannion@gmail.com>
2016-03-02 15:28:15 +0000

    blah

--- week5/connection.php
+++ week5/connection.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1506100
- * Date: 02/03/2016
- * Time: 15:08
- */
 define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
 define('DB_USERNAME', 'bab5a9f1a0500d');
 define('DB_PASSWORD', '7233e6ee');
