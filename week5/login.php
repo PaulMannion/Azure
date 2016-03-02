beb1e81501f9e75d19dbd35b0f2aa4e91de90ca9
PaulMannion <pauld.mannion@gmail.com>
2016-03-02 15:40:24 +0000

    blah

--- week5/login.php
+++ week5/login.php
@@ -13,7 +13,7 @@ include("connection.php"); //Establishing connection with our database
 
 if(empty($_POST["username"]) || empty($_POST["password"]))
 {
-    $echo = "Both fields are required.";
+    echo "Both fields are required.";
 }else
 {
     /**Next we assign local variables to the parameters passed in via the POST
@@ -46,7 +46,7 @@ if(mysqli_num_rows($result) == 1)
     header("location: home.php"); // Redirecting To another Page
 }else
 {
-    $echo "Incorrect username or password.";
+    echo "Incorrect username or password.";
         }
 }
 
