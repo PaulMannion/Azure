ebfeaf02367eb1c8a6f4489656cf4c944bc57877
paulmannion <pauld.mannion@gmail.com>
2016-03-19 17:15:22 +0000

    pass

--- BugTracker/registration/submit.php
+++ BugTracker/registration/submit.php
@@ -5,13 +5,21 @@ if(isset($_POST["submit"]))
     $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
+    $password2 = $_POST["password2"];
 
     $name = mysqli_real_escape_string($db, $name);
     $email = mysqli_real_escape_string($db, $email);
     $password = mysqli_real_escape_string($db, $password);
     $password = md5($password);
 
-
+    // check passwords not the same
+    
+    if ($password != $password2)
+    {
+        throw new Exception('The passwords you entered do not match - please try again.')
+    }
+    
+    
     $sql="SELECT email FROM users WHERE email='$email'";
     $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
