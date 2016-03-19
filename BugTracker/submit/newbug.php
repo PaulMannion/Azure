54e74d5b184c85559a2df53a1ac33352fcf06320
paulmannion <pauld.mannion@gmail.com>
2016-03-19 17:46:19 +0000

    pass

---
+++ BugTracker/submit/newbug.php
@@ -0,0 +1,59 @@
+<?php
+
+	include("connection.php");
+	include("submit.php");
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Log a new bug </title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<form method="post" action="">
+		<fieldset>
+		<legend>New Bug Form</legend>
+			<table width="400" border="0" cellpadding="10" cellspacing="10">
+				<tr>
+					<td colspan="2" align="center" class="error"><?php echo $msg;?></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold">
+							<div align="right"><label for="name">Bug Title</label></div>
+					</td>
+					<td>
+							<input name="title" type="text" class="input" size="25" required />
+					</td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold">
+						<div align="right"><label for="name">Bug Description</label></div>
+					</td>
+					<td>
+						<input name="desc" type="text" class="input" size="500" required />
+					</td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold">
+						<div align="right"><label for="name">Title</label></div>
+					</td>
+					<td>
+						<input name="title" type="text" class="input" size="25" required />
+					</td>
+				</tr>
+				<tr>
+					<td height="23"></td>
+					<td>
+						<div align="right">
+				  		<input type="submit" name="submit" value="Register!" />
+						</div>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
