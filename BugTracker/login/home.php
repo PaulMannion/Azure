b2f51a65ab710b2f4f9f46abc077abf0f48ea00f
paulmannion <1506100@rgu.ac.uk>
2016-03-19 11:52:45 +0000

    sat

--- BugTracker/login/home.php
+++ BugTracker/login/home.php
@@ -9,10 +9,46 @@
 <title>Home</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
-
 <body>
-<h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
-<br><br><br>
-<a href="logout.php" style="font-size:18px">Logout?</a>
+
+<header>
+	<h1>My Bug Tracker Website</h1>
+	<h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
+	<nav>
+		<ul>
+			<li><a href="index.html">Home Page</a></li>
+			<li><a href="logout.php" style="font-size:18px">Logout?</a></li>
+			<li><a href="registration/register.php">Register</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	<h2>Welcome to my Bug Tracker!</h2>
+	<p>This website is used to keep track of all of my bugs</p>
+
+	<div id="bugContainer">
+		<h3>Bug Name 1</h3>
+		<img class="TextWrap" src="assets/images/mug1.png" alt="bug">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+
+	<div id="bugContainer">
+		<h3>Bug Name</h3>
+		<img src="assets/images/mug2.png" alt="bug">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+
+	<div id="bugContainer">
+		<h3>Bug Name</h3>
+		<img src="assets/images/mug3.png" alt="bug">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</main>
+
+<footer>
+	<p>(c) 2016 1506100 Paul Mannion</p>
+</footer>
+
 </body>
 </html>
