1096a9dc28169f4b924a3181924fb0f3a3d3b041
paulmannion <1506100@rgu.ac.uk>
2016-03-21 20:52:28 +0000

    sat

--- BugTracker/addBugReport/submit.php
+++ BugTracker/addBugReport/submit.php
@@ -4,8 +4,8 @@ if(isset($_POST["submit"]))
 {
     $bugTitle = $_POST["bugTitle"];
     $bugDesc = $_POST["bugDesc"];
-    //$postDate = $_
     //$Comment = $_POST["Comment"];
+    $thisUser = $_SESSION['user_id'];
 
     $bugTitle = mysqli_real_escape_string($db, $bugTitle);
     $bugDesc = mysqli_real_escape_string($db, $bugDesc);
@@ -15,7 +15,7 @@ if(isset($_POST["submit"]))
 
 
 
-        $query = mysqli_query($db, "INSERT INTO bugs (title, desc)VALUES ('$bugTitle', '$bugDesc')")or die(mysqli_error($db));
+        $query = mysqli_query($db, "INSERT INTO bugs (title, desc, postDate, userID) VALUES ('$bugTitle', '$bugDesc', current_date(), '$thisUser')")or die(mysqli_error($db));
        // $query = mysqli_query($db, "INSERT INTO users (username, email, password)VALUES ('$name', '$email', '$password')")or die(mysqli_error($db));
         if($query)
         {
