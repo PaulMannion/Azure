cd11c4f3fc8c5e6713614a8bb7ad1fc051cfccad
paulmannion <1506100@rgu.ac.uk>
2016-03-22 21:06:34 +0000

    sat

--- BugTracker/login/loggedin.php
+++ BugTracker/login/loggedin.php
@@ -78,7 +78,11 @@
 				echo "<td>$bugTitle</td>";
 				echo "<td>$bugDesc</td>";
 				echo "<td>$bugPost</td>";
+					if $bugFix = null {
+						echo "<td>Not Fixed</td>";
+					} else {
 				echo "<td>$bugFix</td>";
+				}
 				echo "<td>$bugFixed</td>";
 
 			echo "</tr>\n";
@@ -89,24 +93,6 @@
 
 	</table>
 
-
-	<div id="bugContainer">
-		<h3>Bug Name 1</h3>
-		<img class="TextWrap" src="/BugTracker/assets/images/mug1.png" alt="bug">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
-
-	<div id="bugContainer">
-		<h3>Bug Name</h3>
-		<img src="/BugTracker/assets/images/mug2.png" alt="bug">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
-
-	<div id="bugContainer">
-		<h3>Bug Name</h3>
-		<img src="/BugTracker/assets/images/mug3.png" alt="bug">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
 </main>
 
 <footer>
