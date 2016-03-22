826388a99c2085768ec9e1442bff3db39abe5dca
paulmannion <1506100@rgu.ac.uk>
2016-03-22 20:42:55 +0000

    sat

--- BugTracker/login/loggedin.php
+++ BugTracker/login/loggedin.php
@@ -39,7 +39,7 @@
 		$field = mysqli_fetch_field($result);
 		echo "<td>{$field->name}</td>";
 	}
-	*/echo "</tr>\n";
+	echo "</tr>\n";
 	// printing table rows
 	while($row = mysqli_fetch_row($result))
 	{
@@ -52,68 +52,33 @@
 
 		echo "</tr>\n";
 	}
-
+*/
 	?>
 
 
 	<table id="bugs">
 		<tr>
-			<th>Company</th>
-			<th>Contact</th>
-			<th>Country</th>
+			<th>Bug Title</th>
+			<th>Bug Description</th>
+			<th>Date Posted</th>
+			<th>Date Fixed</th>
+			<th>Fixed (Y or N)</th>
 		</tr>
 		<tr>
-			<td>Alfreds Futterkiste</td>
-			<td>Maria Anders</td>
-			<td>Germany</td>
-		</tr>
-		<tr>
-			<td>Berglunds snabbköp</td>
-			<td>Christina Berglund</td>
-			<td>Sweden</td>
-		</tr>
-		<tr>
-			<td>Centro comercial Moctezuma</td>
-			<td>Francisco Chang</td>
-			<td>Mexico</td>
-		</tr>
-		<tr>
-			<td>Ernst Handel</td>
-			<td>Roland Mendel</td>
-			<td>Austria</td>
-		</tr>
-		<tr>
-			<td>Island Trading</td>
-			<td>Helen Bennett</td>
-			<td>UK</td>
-		</tr>
-		<tr>
-			<td>Königlich Essen</td>
-			<td>Philip Cramer</td>
-			<td>Germany</td>
-		</tr>
-		<tr>
-			<td>Laughing Bacchus Winecellars</td>
-			<td>Yoshi Tannamuri</td>
-			<td>Canada</td>
-		</tr>
-		<tr>
-			<td>Magazzini Alimentari Riuniti</td>
-			<td>Giovanni Rovelli</td>
-			<td>Italy</td>
-		</tr>
-		<tr>
-			<td>North/South</td>
-			<td>Simon Crowther</td>
-			<td>UK</td>
-		</tr>
-		<tr>
-			<td>Paris spécialités</td>
-			<td>Marie Bertrand</td>
-			<td>France</td>
-		</tr>
-	</table>
 
+	<?php
+			while($row = mysqli_fetch_row($result))
+			{
+			foreach($row as $cell)
+			echo "<td>$cell</td>";
+
+			echo "</tr>\n";
+		}
+
+	?>
+		</tr>
+
+	</table>
 
 
 	<div id="bugContainer">
