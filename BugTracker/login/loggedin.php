f69f0a63cae1d2dd4be62bce6d29d021d688c3ad
paulmannion <1506100@rgu.ac.uk>
2016-03-22 21:54:56 +0000

    sat

--- BugTracker/login/loggedin.php
+++ BugTracker/login/loggedin.php
@@ -29,33 +29,9 @@
 	<?php
 	$sql = "SELECT title, bugDesc, postDate, fixDate, fixed FROM bugs";
 	$result=mysqli_query($db,$sql);
-	//$fields_num = mysqli_num_fields($result);
 
-	echo "<h1>Table: Bugs</h1>";
-	echo "<table border='1'><tr>";
-	// printing table headers
-	/*for($i=0; $i<$fields_num; $i++)
-	{
-		$field = mysqli_fetch_field($result);
-		echo "<td>{$field->name}</td>";
-	}
-	echo "</tr>\n";
-	// printing table rows
-	while($row = mysqli_fetch_row($result))
-	{
-		echo "<tr>";
-
-		// $row is array... foreach( .. ) puts every element
-		// of $row to $cell variable
-		foreach($row as $cell)
-			echo "<td>$cell</td>";
-
-		echo "</tr>\n";
-	}
-*/
 	?>
-
-
+	
 	<table id="bugs">
 		<tr>
 			<th>Bug Title</th>
