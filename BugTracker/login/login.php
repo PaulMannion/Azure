208cf71fd0b0df38fd67f97c975f23eed70dcafd
paulmannion <1506100@rgu.ac.uk>
2016-03-23 11:59:27 +0000

    sat

--- BugTracker/login/login.php
+++ BugTracker/login/login.php
@@ -23,7 +23,7 @@
 
 			//Check username and password from database
 			$sql="SELECT approved FROM users WHERE username='$username' and password='$password' limit 1";
-			$result=mysqli_query($db,$sql);
+			$result=mysqli_query($sql);
 			$userApproved=mysqli_fetch_object($result);
 
 			//Check user has been approved before allowing entry
