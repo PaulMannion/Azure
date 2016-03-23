7ba26320411659622a9c4ce244eb62ee6eb16c8b
paulmannion <1506100@rgu.ac.uk>
2016-03-23 21:19:43 +0000

    sat

--- BugTracker/showBugs/showBugs.php
+++ BugTracker/showBugs/showBugs.php
@@ -12,17 +12,17 @@ $id=31;
     $sql = "SELECT * FROM bugs WHERE bugID=" . $id . "";
     $result=mysqli_query($db,$sql);
 
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    $bugTitle = $row['title'];
-    $bugID = $row['bugID'];
-    $bugDesc = $row['desc'];
+    while($row = mysqli_fetch_assoc($result)) {
+        $bugTitle = $row['title'];
+        $bugID = $row['bugID'];
+        $bugDesc = $row['desc'];
 
-    echo $bugTitle;
-    echo $bugDesc;
-    echo "Comeon ye eejit";
-    echo "I was passed the variable" . $id ."";
-    echo "<em>$id<em>";
-
+        echo $bugTitle;
+        echo $bugDesc;
+        echo "Comeon ye eejit";
+        echo "I was passed the variable" . $id . "";
+        echo "<em>$id<em>";
+    }
    // echo '<a href="bug.php?id="'.$bugID.'>'.$bugTitle.'</a></br>';
  ?>
 
