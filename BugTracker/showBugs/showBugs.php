b588fd6f16d6164c4949779f79a401c82994af37
paulmannion <1506100@rgu.ac.uk>
2016-03-23 21:37:54 +0000

    sat

--- BugTracker/showBugs/showBugs.php
+++ BugTracker/showBugs/showBugs.php
@@ -28,29 +28,12 @@
     <p>Here is a list of all the bugs:</p>
 
 
- <?php
-$id=31;
-
-
-    $sql = "SELECT * FROM bugs WHERE bugID=" . $id . "";
-    $result=mysqli_query($db,$sql);
-
-    while($row = mysqli_fetch_assoc($result)) {
-        $bugTitle = $row['title'];
-        $bugID = $row['bugID'];
-        $bugDesc = $row['desc'];
-
-        echo $bugTitle;
-        echo $bugDesc;
-        echo "Comeon ye eejit";
-        echo "I was passed the variable" . $id . "";
-        echo "<em>$id<em>";
-    }
-   // echo '<a href="bug.php?id="'.$bugID.'>'.$bugTitle.'</a></br>';
- ?>
 
     <?php
-    $sql = "SELECT * FROM bugs";
+
+    $id="11";
+
+    $sql="SELECT * FROM bugs WHERE bugID='$id'";
     $result=mysqli_query($db,$sql);
 
     ?>
