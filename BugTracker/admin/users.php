244d2b675c74619f35123db29a96d274f7f0d6e4
paulmannion <1506100@rgu.ac.uk>
2016-03-23 16:06:35 +0000

    sat

--- BugTracker/admin/users.php
+++ BugTracker/admin/users.php
@@ -44,7 +44,7 @@ include("check.php");
         <tr>
 
             <?php
-         //   echo "<script type='text/javascript'>alert('Is it working tho?')</script>";
+            echo "<script type='text/javascript'>alert('$result')</script>";
             while($row = mysqli_fetch_assoc($result)) {
                 $userName = $row['username'];
                 $userMail = $row['email'];
