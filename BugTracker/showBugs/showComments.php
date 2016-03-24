50f743a68556187a74a00a9704efe133f66f41bc
paulmannion <1506100@rgu.ac.uk>
2016-03-24 22:55:16 +0000

    sat

--- BugTracker/showBugs/showComments.php
+++ BugTracker/showBugs/showComments.php
@@ -19,7 +19,7 @@
 
 <header>
     <h1>My Bug Tracker Website</h1>
-    <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
+    <h1 class="hello">Hello, <em><?php echo $login_userID;?>!</em></h1>
     <nav>
         <ul>
             <li><a href="/BugTracker/login/logout.php" style="font-size:18px">Logout?</a></li>
