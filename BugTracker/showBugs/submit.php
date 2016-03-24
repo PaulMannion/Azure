fe3d64a39a3360961e215a04f2ba18ae07306704
paulmannion <1506100@rgu.ac.uk>
2016-03-24 23:12:37 +0000

    sat

--- BugTracker/showBugs/submit.php
+++ BugTracker/showBugs/submit.php
@@ -6,9 +6,9 @@ if(isset($_POST["submit"]))
     $bugID = $_POST["bugID"];
 
 
-    echo "<p>The bugID is: <?php echo $bugID;?></p>";
-    echo "<p>The userID is: <?php echo $login_userID;?></p>";
-    echo "<p>The comment is: <?php echo $desc;?></p>";
+    echo "<p>The bugID is: <?php echo '$bugID';?></p>";
+    echo "<p>The userID is: <?php echo '$login_userID';?></p>";
+    echo "<p>The comment is: <?php echo '$desc';?></p>";
 
     $desc = mysqli_real_escape_string($db, $desc);
 
