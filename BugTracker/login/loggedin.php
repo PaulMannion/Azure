6396d7487deff869795b6c841c04f3c74c8ffca6
paulmannion <1506100@rgu.ac.uk>
2016-03-24 12:10:09 +0000

    sat

--- BugTracker/login/loggedin.php
+++ BugTracker/login/loggedin.php
@@ -129,7 +129,7 @@
 
 					?>
 
-					<td><form name="form" method="POST" action="adm.php">
+					<td><form name="form" method="POST" action="/BugTracker/admin/adm.php">
 						<input value="<?php echo $userID;?>" type="hidden" name="search">
 						<input type="submit" value="Make Admin">
 					</form></td>
