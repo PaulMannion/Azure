8811b8cc660c699e8da9bd7aa6c396872e7deecd
paulmannion <1506100@rgu.ac.uk>
2016-03-24 14:26:54 +0000

    sat

--- BugTracker/login/loggedin.php
+++ BugTracker/login/loggedin.php
@@ -139,7 +139,7 @@
 					<td><form name="form" method="POST" action="/BugTracker/admin/adm.php">
 						<input value="<?php echo $userID;?>" type="hidden" name="id">
 							<input value="<?php echo $userName;?>" type="hidden" name="name">
-						<input type="submit" value="Make Admin" style="color:#4CAF50!important">
+						<input type="submit" value="Make Admin" style="background-color:#4CAF50!important">
 					</form></td>
 
 					<?php
