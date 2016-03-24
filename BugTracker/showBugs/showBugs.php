c232cfa845cc4f2020bc5e22b59d8c1b144a4018
paulmannion <1506100@rgu.ac.uk>
2016-03-24 22:18:56 +0000

    sat

--- BugTracker/showBugs/showBugs.php
+++ BugTracker/showBugs/showBugs.php
@@ -17,8 +17,9 @@
     <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
     <nav>
         <ul>
-            <li><a href="logout.php" style="font-size:18px">Logout?</a></li>
+            <li><a href="/BugTracker/login/logout.php" style="font-size:18px">Logout?</a></li>
             <li><a href="/BugTracker/addBugReport/newbug.php">Submit New Bug Report</a></li>
+            <li><a href="/BugTracker/showBugs/showBugs.php" style="font-size:18px">Show Bug Report</a></li>
         </ul>
     </nav>
 </header>
@@ -59,7 +60,7 @@
                 $bugFixed = $row['fixed'];
                 $bugAuth = $row['isFixed'];
 
-                
+
                 echo "<td>$bugTitle</td>";
                 echo "<td>$bugDesc</td>";
                 echo "<td>$bugPost</td>";
