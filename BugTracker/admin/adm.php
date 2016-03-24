d05ab661d4cc9d5c75e698415db714f5918ac3a6
paulmannion <1506100@rgu.ac.uk>
2016-03-24 12:13:46 +0000

    sat

--- BugTracker/admin/adm.php
+++ BugTracker/admin/adm.php
@@ -9,6 +9,26 @@
     //$result=mysqli_query($db,$sql);
 
 
-echo "<h1> $id<h1>";
 ?>
 
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<header>
+    <h1>My Bug Tracker Website</h1>
+    <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
+    <nav>
+        <ul>
+            <li><a href="/BugTracker/login/logout.php" style="font-size:18px">Logout?</a></li>
+            <li><a href="/BugTracker/addBugReport/newbug.php">Submit New Bug Report</a></li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
