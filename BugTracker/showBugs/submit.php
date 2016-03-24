3bdb29b1e0c0151ea90a90d28fc9a658f717e3e9
paulmannion <1506100@rgu.ac.uk>
2016-03-24 23:57:13 +0000

    sat

--- BugTracker/showBugs/submit.php
+++ BugTracker/showBugs/submit.php
@@ -7,13 +7,13 @@ if(isset($_POST["submit"]))
 
     $comDesc = mysqli_real_escape_string($db, $comDesc);
 
-    echo "<p>The bugID is: $bugID</p>";
-    echo "<p>The userID is: $login_userID</p>";
-    echo "<p>The comment is: $comDesc</p>";
+  //  echo "<p>The bugID is: $bugID</p>";
+  //  echo "<p>The userID is: $login_userID</p>";
+  //  echo "<p>The comment is: $comDesc</p>";
 
 
 
-/*
+
             $query = mysqli_query($db, "INSERT INTO comments (comText, postDate, userID, bugID) VALUES ('$comDesc', current_date(), $login_userID, $bugID)") or die(mysqli_error($db));
             if ($query) {
                 
@@ -24,6 +24,6 @@ if(isset($_POST["submit"]))
         else
         {
             $msg = "Sorry...Something Terrible has Happened...";
- */       }
+        }
 
 ?>
