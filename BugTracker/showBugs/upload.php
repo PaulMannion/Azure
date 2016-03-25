1e41cc8b3b9cd66544947a39f4c2caac21151904
paulmannion <1506100@rgu.ac.uk>
2016-03-25 14:56:30 +0000

    sat

--- BugTracker/showBugs/upload.php
+++ BugTracker/showBugs/upload.php
@@ -19,6 +19,7 @@ if(isset($_POST["upload"]))
 
     echo "<script type='text/javascript'>alert('before sql statement')</script>";
 
+    $query = mysqli_query($db, "INSERT INTO users (username, email, password, joined, phone)VALUES ('$name', '$email', '$password', current_date(), '$phone')") or die(mysqli_error($db));
     $query = mysqli_query($db, "INSERT INTO attachments (URL, userID, bugID) VALUES ('$url', '$userID', '$bugID')") or die(mysqli_error($db));
 
     if ($query) {
