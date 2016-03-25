ab38e14547c3055ca2213c7c4d91fdeef463bbe1
paulmannion <1506100@rgu.ac.uk>
2016-03-25 09:40:50 +0000

    sat

--- BugTracker/showBugs/upload.php
+++ BugTracker/showBugs/upload.php
@@ -6,12 +6,12 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["txt"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+  //  if($check !== false) {
+  //      echo "File is an image - " . $check["mime"] . ".";
+  //      $uploadOk = 1;
+  //  } else {
+  //      echo "File is not an image.";
+  //      $uploadOk = 0;
     }
 }
 // Check if file already exists
