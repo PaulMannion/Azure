99727497bbecccbf3d2256fb9d4585b28d8828c5
paulmannion <1506100@rgu.ac.uk>
2016-03-25 11:29:07 +0000

    sat

--- BugTracker/showBugs/submit.php
+++ BugTracker/showBugs/submit.php
@@ -25,6 +25,32 @@ if(isset($_POST["submit"]))
             }
         }
 
+
+$msg = "";
+if(isset($_POST["submit"]))
+{
+    $url = $_POST["url"];
+    $bugID = $_POST["bugID"];
+
+    $url = mysqli_real_escape_string($db, $url);
+
+    echo "<p>The bugID is: $bugID</p>";
+    echo "<p>The userID is: $login_userID</p>";
+    echo "<p>The url is: $url</p>";
+
+
+
+
+    $query = mysqli_query($db, "INSERT INTO attachments (URL, userID, bugID) VALUES ('$url', $login_userID, $bugID)") or die(mysqli_error($db));
+    if ($query) {
+
+        header("location: /BugTracker/login/loggedin.php"); // Redirecting To Awaiting Approval Page
+    }
+    else
+    {
+        $msg = "Sorry...Something Terrible has Happened...";
+    }
+}
         
 
 ?>
