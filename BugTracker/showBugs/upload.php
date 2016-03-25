bbb11e73c18fbea10e1942311a051f645abc7259
paulmannion <1506100@rgu.ac.uk>
2016-03-25 14:44:48 +0000

    sat

--- BugTracker/showBugs/upload.php
+++ BugTracker/showBugs/upload.php
@@ -3,9 +3,13 @@
 $msg = "";
 if(isset($_POST["upload"]))
 {
-    $url = $_POST["url"];
-    $bugID = $_POST["bugID"];
-    $userID = $_POST["userID"];
+ //   $url = $_POST["url"];
+ //   $bugID = $_POST["bugID"];
+ //   $userID = $_POST["userID"];
+
+    $url = "blah";
+    $bugID = 41;
+    $userID = 21;
 
     //$url = mysqli_real_escape_string($db, $url);
 
@@ -15,8 +19,8 @@ if(isset($_POST["upload"]))
 
 
 
-    //  $query = mysqli_query($db, "INSERT INTO attachments (URL, userID, bugID) VALUES ($url, $userID, $bugID)") or die(mysqli_error($db));
-        $query = mysqli_query($db, "INSERT INTO attachments (URL, userID, bugID) VALUES ('httpblah', 21, 41)") or die(mysqli_error($db));
+    $query = mysqli_query($db, "INSERT INTO attachments (URL, userID, bugID) VALUES ($url, $userID, $bugID)") or die(mysqli_error($db));
+
     if ($query) {
         
         echo "<script type='text/javascript'>alert('Attachment Successfully Uploaded')</script>";
