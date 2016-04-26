9770ac2eef82bade258af13bc6e8db9eba1d0a58
paulmannion <1506100@rgu.ac.uk>
2016-04-26 19:55:04 +0100

    sat

---
+++ week11/connection.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b03c736b29bc97');
+define('DB_PASSWORD', '3885262f');
+define('DB_DATABASE', 'vulnerable_1506100');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
