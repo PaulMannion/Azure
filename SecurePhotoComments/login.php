dde700a1df67450ca5789a5b0dfc17e03f43d709
paulmannion <1506100@rgu.ac.uk>
2016-04-27 22:26:48 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -53,32 +53,32 @@ error_reporting(E_ALL);
 
 
 	if(isset($_POST["submit"])) {
-		if (empty($_POST["username"]) || empty($_POST["password"])) {
-			$error = "Both fields are required.";
-		} else {
-			// Check Anti-CSRF token
-			//checkToken($_REQUEST['user_token'], $_SESSION['session_token'], 'index.php');
+        if (empty($_POST["username"]) || empty($_POST["password"])) {
+            $error = "Both fields are required.";
+        } else {
+            // Check Anti-CSRF token
+            //checkToken($_REQUEST['user_token'], $_SESSION['session_token'], 'index.php');
 
-			// Sanitise username input
-			$user = $_POST['username'];
-			$user = stripslashes($user);
-			$user = mysqli_real_escape_string($db,$user);
+            // Sanitise username input
+            $user = $_POST['username'];
+            $user = stripslashes($user);
+            $user = mysqli_real_escape_string($db, $user);
 
-			// Sanitise password input
-			$pass = $_POST['password'];
-			$pass = stripslashes($pass);
-			$pass = mysqli_real_escape_string($db,$pass);
-			$pass = md5($pass);
+            // Sanitise password input
+            $pass = $_POST['password'];
+            $pass = stripslashes($pass);
+            $pass = mysqli_real_escape_string($db, $pass);
+            $pass = md5($pass);
 
-			// Default values
-			$total_failed_login = 3;
-			$lockout_time = 15;
-			$account_locked = false;
+            // Default values
+            $total_failed_login = 3;
+            $lockout_time = 15;
+            $account_locked = false;
 
-			// Check the database (Check user information)
+            // Check the database (Check user information)
             // Create a prepared statement //
 
-            $stmt =  $db->stmt_init();
+            $stmt = $db->stmt_init();
             if ($stmt->prepare("SELECT failed_login, last_login FROM users WHERE username =?")) {
 
                 /* bind parameters for markers */
@@ -97,7 +97,7 @@ error_reporting(E_ALL);
 
                 // Check if user has had max number of login attempts
 
-                if ($failed_login >= $total_failed_login){
+                if ($failed_login >= $total_failed_login) {
                     // User is locked out
                     echo "<pre><br />This account has been locked due to too many incorrect logins.</pre>";
 
@@ -117,78 +117,88 @@ error_reporting(E_ALL);
 
             echo "<p><em>Warning</em>: Success!.</p>";
 
-/*
-            $data = $db->prepare('SELECT failed_login, last_login FROM users WHERE username = (:user) LIMIT 1;');
-			var_dump($data);
-			$data->bind_Param(':user', $user, PDO::PARAM_STR);  //PDO is a pain in the arse
-			$data->execute();
-			$row = $data->fetch();
-			echo "<p><em>Warning</em>: WE got as far fetching data.</p>";
-			// Check to see if the user has been locked out.
-			if (($data->rowCount() == 1) && ($row['failed_login'] >= $total_failed_login)) {   // no rowcount method in mysql - fix connection.php as per lecture slides creating stored proc
-				// User locked out.  Note, using this method would allow for user enumeration!
-				//echo "<pre><br />This account has been locked due to too many incorrect logins.</pre>";
+            /*
+                        $data = $db->prepare('SELECT failed_login, last_login FROM users WHERE username = (:user) LIMIT 1;');
+                        var_dump($data);
+                        $data->bind_Param(':user', $user, PDO::PARAM_STR);  //PDO is a pain in the arse
+                        $data->execute();
+                        $row = $data->fetch();
+                        echo "<p><em>Warning</em>: WE got as far fetching data.</p>";
+                        // Check to see if the user has been locked out.
+                        if (($data->rowCount() == 1) && ($row['failed_login'] >= $total_failed_login)) {   // no rowcount method in mysql - fix connection.php as per lecture slides creating stored proc
+                            // User locked out.  Note, using this method would allow for user enumeration!
+                            //echo "<pre><br />This account has been locked due to too many incorrect logins.</pre>";
 
-				// Calculate when the user would be allowed to login again
-				$last_login = $row['last_login'];
-				$last_login = strtotime($last_login);
-				$timeout = strtotime("{$last_login} +{$lockout_time} minutes");
-				$timenow = strtotime("now");
+                            // Calculate when the user would be allowed to login again
+                            $last_login = $row['last_login'];
+                            $last_login = strtotime($last_login);
+                            $timeout = strtotime("{$last_login} +{$lockout_time} minutes");
+                            $timenow = strtotime("now");
 
-				// Check to see if enough time has passed, if it hasn't locked the account
-				if ($timenow > $timeout)
-					$account_locked = true;
-			}
-*/
-			// Check the database (if username matches the password)
+                            // Check to see if enough time has passed, if it hasn't locked the account
+                            if ($timenow > $timeout)
+                                $account_locked = true;
+                        }
+            */
+            // Check the database (if username matches the password)
 
             $query = $db->prepare("SELECT * FROM users where username=? AND password=?");
-            $query -> bind_param("ss", $user, $pass);
+            $query->bind_param('ss', $user, $pass);
+            $query->execute();
+            $query->bind_result($username, $password);
+            $query->store_result();
+            if ($query->num_rows == 1) //check a user was found
+            {
+                if ($query->fetch()) // fetch contents of row
+                {
+                    if ($account_locked == false) {
+                        // Login successful
+                        $_SESSION['username'] = $username; // Initializing Session
+                        header("location: photos.php"); // Redirecting To Other Page
 
-            if($query->execute() == true) {
+                        // Had the account been locked out since last login?
+                        if ($failed_login >= $total_failed_login) {
+                            echo "<p><em>Warning</em>: Someone might of been brute forcing your account.</p>";
+                            echo "<p>Number of login attempts: <em>{$failed_login}</em>.<br />Last login attempt was at: <em>${last_login}</em>.</p>";
+                        }
 
-                $query->bind_result($result);
-                $query -> fetch();
+                        // Reset bad login count
+                        $data = $db->prepare('UPDATE users SET failed_login = "0" WHERE username=?');
+                        $data->bindParam("ss", $user);
+                        $data->execute();
+                    } else {
+                        // Login failed
+                        sleep(rand(2, 4));
 
-            if(($query->execute() == true) && ($account_locked == false)) {
-                // Login successful
-                $_SESSION['username'] = $user; // Initializing Session
-                header("location: photos.php"); // Redirecting To Other Page
+                        // Give the user some feedback
+                        $error = "<pre><br />Username and/or password incorrect.<br /><br/>Alternative, the account has been locked because of too many failed logins.<br />If this is the case, <em>please try again in {$lockout_time} minutes</em>.</pre>";
 
-                // Had the account been locked out since last login?
-                if ($failed_login >= $total_failed_login) {
-                    echo "<p><em>Warning</em>: Someone might of been brute forcing your account.</p>";
-                    echo "<p>Number of login attempts: <em>{$failed_login}</em>.<br />Last login attempt was at: <em>${last_login}</em>.</p>";
+                        // Update bad login count
+                        $data = $db->prepare('UPDATE users SET failed_login = (failed_login + 1) WHERE username=?');
+                        $data->bind_Param("ss", $user);
+                        $data->execute();
+                    }
+
+                    // Set the last login time
+                    $data = $db->prepare('UPDATE users SET last_login = now() WHERE username=?');
+                    $data->bindParam("ss", $user);
+                    $data->execute();
                 }
 
-                // Reset bad login count
-                $data = $db->prepare('UPDATE users SET failed_login = "0" WHERE username=?');
-                $data->bindParam("ss", $user);
-                $data->execute();
-
-            } else{
-                // Login failed
-                sleep(rand(2, 4));
-
-                // Give the user some feedback
-                $error = "<pre><br />Username and/or password incorrect.<br /><br/>Alternative, the account has been locked because of too many failed logins.<br />If this is the case, <em>please try again in {$lockout_time} minutes</em>.</pre>";
-
-                // Update bad login count
-                $data = $db->prepare('UPDATE users SET failed_login = (failed_login + 1) WHERE username=?');
-                $data->bind_Param("ss", $user);
-                $data->execute();
+                $stmt->close();
             }
-
-                // Set the last login time
-                $data = $db->prepare('UPDATE users SET last_login = now() WHERE username=?');
-                $data->bindParam("ss", $user);
-                $data->execute();
-            }
-
-            $stmt->close();
+            $db->close();
         }
 
     }
+
+
+
+
+
+
+
+
 
 /*			$data = $db->prepare('SELECT * FROM users WHERE user = (:user) AND password = (:password) LIMIT 1;');
 			$data->bindParam(':user', $user, PDO::PARAM_STR);
