0f00d2132d6e3b6e02d6f7a405a0a0513578a97f
paulmannion <1506100@rgu.ac.uk>
2016-04-28 21:24:54 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -122,7 +122,7 @@ error_reporting(E_ALL);
                     if ($timenow > $timeout) {
                     $account_locked = true;
 
-                    echo "<p>(timeout={$timeout} timenow= {$timenow} This will only appear if user attempts is greater thane etc Number of login attempts: <em>{$failed_login}</em> Acount lock status is: {$account_locked} .<br />Last login attempt was at: <em>${last_login}</em>.</p>";
+                    echo "<p>(timeout={$timeout} timenow= {$timenow} Time remaining = .({$timeout}-{timenow}). This will only appear if user attempts is greater thane etc Number of login attempts: <em>{$failed_login}</em> Acount lock status is: {$account_locked} .<br />Last login attempt was at: <em>${last_login}</em>.</p>";
                     var_dump($account_locked);
                     }
                 }
