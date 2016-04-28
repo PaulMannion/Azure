daaf6a92bd18085570cf237fc37f78a0eed18ff7
paulmannion <1506100@rgu.ac.uk>
2016-04-28 21:25:09 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -119,12 +119,12 @@ error_reporting(E_ALL);
                     var_dump($timenow);
 
                     // Check to see if enough time has passed, if it hasn't locked the account
-                    if ($timenow > $timeout) {
+                    if ($timenow > $timeout)
                     $account_locked = true;
 
                     echo "<p>(timeout={$timeout} timenow= {$timenow} Time remaining = .({$timeout}-{timenow}). This will only appear if user attempts is greater thane etc Number of login attempts: <em>{$failed_login}</em> Acount lock status is: {$account_locked} .<br />Last login attempt was at: <em>${last_login}</em>.</p>";
                     var_dump($account_locked);
-                    }
+                    
                 }
 
                 echo "<pre><br />This part means you are a user who entered an incorrect password <em>{$failed_login}</em> but not more than max.</pre>";
