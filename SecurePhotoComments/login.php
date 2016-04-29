87b44c7b713545344c2b75fade7b495e73a1274f
paulmannion <1506100@rgu.ac.uk>
2016-04-29 10:18:17 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -114,7 +114,7 @@ error_reporting(E_ALL);
                     $timeout = ($last_login + $lockout_time);
                     // $timeout = strtotime("{$last_login} +{$lockout_time} minutes");
                     $timenow = strtotime("now");
-                    $timeleft = (round(($timeout-$timenow)/60)*60);
+                    $timeleft = (round(($timeout-$timenow)/3600));
 
                     var_dump($timeout);
                     var_dump($timenow);
