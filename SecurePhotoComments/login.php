58fd6bc3f038e4ddd9ebaf89f8c56e7743dff0dd
paulmannion <1506100@rgu.ac.uk>
2016-04-29 16:06:29 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -291,60 +291,7 @@ error_reporting(E_ALL);
 
     }
 
-
-
-
-
-
-
-
-
-/*			$data = $db->prepare('SELECT * FROM users WHERE user = (:user) AND password = (:password) LIMIT 1;');
-			$data->bindParam(':user', $user, PDO::PARAM_STR);
-			$data->bindParam(':password', $pass, PDO::PARAM_STR);
-			$data->execute();
-			$row = $data->fetch();
-
-			// If its a valid login...
-			if (($data->rowCount() == 1) && ($account_locked == false)) {
-				// Get users details
-				$avatar = $row['avatar'];
-				$failed_login = $row['failed_login'];
-				$last_login = $row['last_login'];
-
-				// Login successful
-				$_SESSION['username'] = $user; // Initializing Session
-				header("location: photos.php"); // Redirecting To Other Page
-
-				// Had the account been locked out since last login?
-				if ($failed_login >= $total_failed_login) {
-					echo "<p><em>Warning</em>: Someone might of been brute forcing your account.</p>";
-					echo "<p>Number of login attempts: <em>{$failed_login}</em>.<br />Last login attempt was at: <em>${last_login}</em>.</p>";
-				}
-
-				// Reset bad login count
-				$data = $db->prepare('UPDATE users SET failed_login = "0" WHERE user = (:user) LIMIT 1;');
-				$data->bindParam(':user', $user, PDO::PARAM_STR);
-				$data->execute();
-			} else {
-				// Login failed
-				sleep(rand(2, 4));
-
-				// Give the user some feedback
-				$error = "<pre><br />Username and/or password incorrect.<br /><br/>Alternative, the account has been locked because of too many failed logins.<br />If this is the case, <em>please try again in {$lockout_time} minutes</em>.</pre>";
-
-				// Update bad login count
-				$data = $db->prepare('UPDATE users SET failed_login = (failed_login + 1) WHERE user = (:user) LIMIT 1;');
-				$data->bindParam(':user', $user, PDO::PARAM_STR);
-				$data->execute();
-			}
-
-			// Set the last login time
-			$data = $db->prepare('UPDATE users SET last_login = now() WHERE user = (:user) LIMIT 1;');
-			$data->bindParam(':user', $user, PDO::PARAM_STR);
-			$data->execute();
-		}
-
+/*
 		// Generate Anti-CSRF token
 		//generateSessionToken();
 	}
