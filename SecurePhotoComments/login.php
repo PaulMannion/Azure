92f927f5750cd826685d5166fab38f7c3bc563b6
paulmannion <1506100@rgu.ac.uk>
2016-04-29 12:29:17 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -78,6 +78,7 @@ error_reporting(E_ALL);
          //   echo "<p>Has password been hashed? <em>{$pass}</em></p>";
 
             // Default values
+            date_default_timezone_set('GMT');
             $total_failed_login = 3;
             $lockout_time = 15;
             $account_locked = false;
