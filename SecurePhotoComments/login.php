c6ac74a68537fabddf73c94bcbd287e1ffd854ee
paulmannion <1506100@rgu.ac.uk>
2016-04-29 16:20:57 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -1,46 +1,4 @@
 <?php
-/*
-	session_start();
-	include("connection.php"); //Establishing connection with our database
-	
-	$error = ""; //Variable for storing our errors.
-	if(isset($_POST["submit"]))
-	{
-		if(empty($_POST["username"]) || empty($_POST["password"]))
-		{
-			$error = "Both fields are required.";
-		}else
-		{
-			// Define $username and $password
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-
-
-			
-			//Check username and password from database
-			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql);
-			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
-			
-			//If username and password exist in our database then create a session.
-			//Otherwise echo error.
-			
-			if(mysqli_num_rows($result) == 1)
-			{
-				$_SESSION['username'] = $username; // Initializing Session
-				header("location: photos.php"); // Redirecting To Other Page
-			}else
-			{
-				$error = "Incorrect username or password.";
-			}
-
-		}
-	}
-*/
-?>
-
-
-<?php
 	//display error
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
@@ -121,7 +79,7 @@ error_reporting(E_ALL);
 
                  //   $last_login = new DateTime(date('h:i:s'));
 
-                 //   echo $last_login->format('h:i:s');
+                    echo $last_login->format('h:i:s');
                 //    $last_login->modify('+15 minutes');
                  //   echo $last_login->format('h:i:s');
 
@@ -282,10 +240,6 @@ error_reporting(E_ALL);
            // $error = "Incorrect username or password.";
             
             $db->close();
-            
-            
-            
-            
             
         }
 
