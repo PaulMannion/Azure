759f554896acb0031ff49cd77ed0c866ec8a3bc4
paulmannion <1506100@rgu.ac.uk>
2016-04-29 20:47:13 +0100

    sat

--- SecurePhotoComments/login.php
+++ SecurePhotoComments/login.php
@@ -73,7 +73,7 @@ error_reporting(E_ALL);
                     echo "<p>This should only print if you've entered a correct username</p>";
                     /* fetch values */
                                  while ($stmt->fetch()) {
-                                      printf("%s %s\n", $failed_login, $last_login, $try_login);
+                                      printf("%s %s %s\n", $failed_login, $last_login, $try_login);
                                   }
 
                     // Check if user has had max number of login attempts
