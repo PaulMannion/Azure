edfadffed71c8351091b50c72829288a01acf13c
paulmannion <1506100@rgu.ac.uk>
2016-04-30 17:03:07 +0100

    pat

--- SecurePhotoComments/submit.php
+++ SecurePhotoComments/submit.php
@@ -20,7 +20,10 @@ if(isset($_POST["submit"]))
 
 
     //   $pass = md5($pass);
-    
+
+    echo "<p>Has username been cleaned? <em>{$user}</em></p>";
+    echo "<p>Has email been cleaned? <em>{$email}</em></p>";
+    echo "<p>Has password been cleaned? <em>{$pass}</em></p>";
     
 
     $sql="SELECT email FROM users WHERE email='$email'";
