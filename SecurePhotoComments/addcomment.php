cb4a8349f25b17c24b5130ab88985523c54fa653
paulmannion <1506100@rgu.ac.uk>
2016-04-30 18:12:05 +0100

    pat

--- SecurePhotoComments/addcomment.php
+++ SecurePhotoComments/addcomment.php
@@ -6,25 +6,63 @@ $msg = ""; //Variable for storing our errors.
 if(isset($_POST["submit"]))
 {
 
-    $desc = $_POST["desc"];
-    $photoID = $_POST["photoID"];
-    $name = $_SESSION["username"];
+    // Sanitise description
+    $desc = $_POST['desc'];
+    $desc = stripslashes($desc);
+    $desc = mysqli_real_escape_string($db, $desc);
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) == 1) {
-        //echo $name." ".$email." ".$password;
-        $id = $row['userID'];
-        $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
-        $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
-        if ($query) {
-            $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
-        }
+    // Sanitise photoID
+    $photoID = $_POST["photoID"];
+    $photoID = stripslashes($photoID);
+    $photoID = mysqli_real_escape_string($db, $photoID);
+
+    $name = $_SESSION["username"];
+    $postDate = now();
+
+    $stmt = $db->stmt_init();
+    $stmt->prepare("SELECT userID FROM users WHERE username =?");
+
+    /* bind parameters for markers */
+    $stmt->bind_param('s', $name);
+
+    /* execute query */
+    $stmt->execute();
+
+    if ($stmt) {
+        //                        print 'Success! The user query ran OK';
+    } else {
+        print 'Error : (' . $db->errno . ') ' . $db->error;
     }
+
+    /* bind variables to prepared statement */
+    $stmt->bind_result($id);
+
+    $stmt->store_result();
+
+    if ($stmt->num_rows == 1) //check a user was found
+    {
+        $stmt = mysqli_prepare($db, "INSERT INTO comments VALUES (?, ?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, 'ssii', $desc, $postDate, $photoID, $id);
+
+        /* execute prepared statement */
+        mysqli_stmt_execute($stmt);
+
+        $msg = "Thank You! comment added. click <a href='photo.php?id=\".$photoID.\"'>here</a> to go back";
+
+        /* close statement and connection */
+        mysqli_stmt_close($stmt);
+
+
+        /* close connection */
+        mysqli_close($link);
+
+    }
+
     else{
         $msg = "You need to login first";
-    }
+        }
+
 }
 
 ?>
+
--- SecurePhotoComments/submit.php
+++ SecurePhotoComments/submit.php
@@ -2,9 +2,9 @@
 $msg = "";
 if(isset($_POST["submit"])) {
     // Sanitise username input
-    $name = $_POST['username'];
-    $name = stripslashes($name);
-    $name = mysqli_real_escape_string($db, $name);
+    $user = $_POST['username'];
+    $user = stripslashes($name);
+    $user = mysqli_real_escape_string($db, $user);
 
     // Sanitise email input
     $email = $_POST["email"];
@@ -68,24 +68,5 @@ if(isset($_POST["submit"])) {
 
     }
 }
-/*
-    $sql="SELECT email FROM users WHERE email='$email'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) == 1)
-    {
-        $msg = "Sorry...This email already exists...";
-    }
-    else
-    {
-        //echo $name." ".$email." ".$password;
-        $query = mysqli_query($db, "INSERT INTO users (username, email, password) VALUES ('$name', '$email', '$password')")or die(mysqli_error($db));
-        if($query)
-        {
-            $msg = "Thank You! you are now registered. click <a href='index.php'>here</a> to login";
-        }
 
-    }
-}
-*/
 ?>
