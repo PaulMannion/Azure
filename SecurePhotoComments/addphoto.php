74676619fbc6cf292e973655339142061348736c
paulmannion <1506100@rgu.ac.uk>
2016-05-01 16:04:14 +0100

    pat

--- SecurePhotoComments/addphoto.php
+++ SecurePhotoComments/addphoto.php
@@ -35,6 +35,33 @@ if(isset($_POST["submit"]))
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     $uploadOk = 1;
 
+        // Check if image file is a actual image or fake image
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+    // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+    // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
 
     $stmt = $db->stmt_init();
     $stmt->prepare("SELECT userID FROM users WHERE username =?");
@@ -85,30 +112,37 @@ if(isset($_POST["submit"]))
                         var_dump($postDate);
                         var_dump($target_file);
                         var_dump($id);
-                        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
-                                    /* Create the prepared statement */
-                                    if ($query = $db->prepare("INSERT INTO photos (title, description, postDate, url, userID) values (?, ?, now(), ?, ?)")) {
+                        // Check if $uploadOk is set to 0 by an error
+                        if ($uploadOk == 0) {
+                            echo "Sorry, your file was not uploaded.";
+                            // if everything is ok, try to upload file
+                        }else{
+                            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
-                                        /* Bind our params */
-                                        $query->bind_param('sssi', $title, $desc, $target_file, $id);
+                                /* Create the prepared statement */
+                                if ($query = $db->prepare("INSERT INTO photos (title, description, postDate, url, userID) values (?, ?, now(), ?, ?)")) {
+
+                                    /* Bind our params */
+                                    $query->bind_param('sssi', $title, $desc, $target_file, $id);
 
 
-                                        /* Execute the prepared Statement */
-                                        $query->execute();
+                                    /* Execute the prepared Statement */
+                                    $query->execute();
 
-                                        echo "Inserted {$title},{$desc},{$postDate},{$target_file},{$id} into database\n";
+                                    echo "Inserted {$title},{$desc},{$postDate},{$target_file},{$id} into database\n";
 
-                                        $msg = "Thank You! The pile " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded. click <a href='photos.php'>here</a> to go back";
+                                    $msg = "Thank You! The pile " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded. click <a href='photos.php'>here</a> to go back";
 
-                                        /* Close the statement */
-                                        $query->close();
-                                    }
-                        }
-                        else {
-                            /* Error */
-                            $msg = "Sorry, there was an error uploading your file.";
+                                    /* Close the statement */
+                                    $query->close();
+                                }
+                            }
+                            else {
+                                /* Error */
+                                $msg = "Sorry, there was an error uploading your file.";
 
+                            }
                         }
 
                         /* close statement and connection */
